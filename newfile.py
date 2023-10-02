ec87dd5c36bcbe40f1f2dc28f092b8b93d84332b
Luke Hand <handl029@gmail.com>
2023-10-02 15:04:26 +0100

    Fixes errors in newfile.py

--- newfile.py
+++ newfile.py
@@ -1,4 +1,4 @@
-var1 = int(input("Give a number and press enter")
-var2 = int(input("Give a second number and press enter")
+var1 = int(input("Give a number and press enter"))
+var2 = int(input("Give a second number and press enter"))
 result = var1 * var2
 print(result)
